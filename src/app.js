0870979c13ce9fbd77c372bc110ac385863a6e46
terry howell <63936327+terrylh22@users.noreply.github.com>
2023-12-26 20:38:01 +0000

    finished excuse generator on refresh, need to do on button click next

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.floor(Math.random() * 200);
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+
+let generateExcuse = () => {
+  //declare sentence piece arrays
+  let pronoun = ["A", "The", "My"];
+  let subject = ["dog", "cat", "car", "plane", "porcupine", "bear"];
+  let verb = ["ate", "blew up", "ran over", "broke", "ruined", "flew into"];
+  let possession = [
+    "my laptop",
+    "my homework",
+    "my house",
+    "my dinner",
+    "my couch",
+    "my playstation"
+  ];
+
+  //declare string variable
+  let generatedExcuse = "";
+
+  //use Math.random to generate a number for each array
+  let randomPronoun = Math.floor(Math.random() * pronoun.length);
+  let randomSubject = Math.floor(Math.random() * subject.length);
+  let randomVerb = Math.floor(Math.random() * verb.length);
+  let randomPossession = Math.floor(Math.random() * possession.length);
+
+  generatedExcuse = `${pronoun[randomPronoun]} ${subject[randomSubject]} ${verb[randomVerb]} ${possession[randomPossession]}.`;
+
+  return generatedExcuse;
+};
+
+//excuse example = A dog ate my homework!
